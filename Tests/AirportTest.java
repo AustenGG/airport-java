ca5fde47ffc6fe693122f0f48db6d2e1bccb6343
Austen Grice-Griffin <austen.grice-griffin@Austens-MacBook-Pro.local>
2018-11-28 11:09:57 +0000

    first test -  default capacity

--- Tests/AirportTest.java
+++ Tests/AirportTest.java
@@ -1,5 +1,19 @@
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
+
 public class AirportTest {
+
+    @Test
+    public void returnCapacity() throws Exception {
+
+        Airport test1 = new Airport();
+        test1.initialize();
+        String result = test1.returnCapacity();
+        assertEquals("The airfield capacity is 100", result);
+
+    }
 
 }
