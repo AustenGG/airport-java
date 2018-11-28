38c37d22efbd5606c2de025c9a10b78757f469f4
Austen Grice-Griffin <austen.grice-griffin@Austens-MacBook-Pro.local>
2018-11-28 11:08:49 +0000

    first test -  default capacity

--- src/Airport.java
+++ src/Airport.java
@@ -1,2 +1,17 @@
 public class Airport {
+    private int capacity;
+
+    public static void main(String args[]) {
+    }
+
+    public void initialize(){
+        this.capacity = 100;
+    }
+
+    public String returnCapacity() {
+
+        return "The airfield capacity is " + this.capacity;
+
+    }
+
 }
